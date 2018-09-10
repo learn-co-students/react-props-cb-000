07be7df52a5b4c2d72715577ae0acac913b6c7bc
Alex Segers <a.segers.dev@gmail.com>
2018-09-10 21:23:59 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,34 @@
-// Code goes here
+import React from 'react'
+
+import ReactDOM from 'react-dom'
+
+class MovieCard extends React.Component {
+  render() {
+    return (
+      <div className="movie-card">
+        <img
+        src={this.props.poster}
+        alt={this.props.title}
+        />
+        <h2>{this.props.title}</h2>
+        <small>Genres: {this.props.genres.join(', ')}</small>
+      </div>
+    )
+
+  }
+}
+
+MovieCard.defaultProps = {
+  poster: 'http://i.imgur.com/bJw8ndW.png'
+};
+
+const jurassicWorldGenres = ['Action', 'Adventure', 'Science Fiction', 'Thriller'];
+
+ReactDOM.render(
+  <MovieCard
+    // The default value of the poster property will appear.
+    title="Jurassic World"
+    genres={jurassicWorldGenres}
+  />,
+  document.getElementById('root')
+);
